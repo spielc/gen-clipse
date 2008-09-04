6069d03281807c018b3eece84ea04208eae3bcc5
spielc <spielc@9c6302da-0c56-0410-948d-e3f9013fe695>
2008-09-04 13:57:14 +0000

    initial import of com.google.code.gen-clipse.debug.core

---
+++ trunk/gen-clipse/com.google.code.gen-clipse.debug.core/src/com/google/code/genclipse/debug/core/launching/EbuildDebuggingConstants.java
@@ -0,0 +1,29 @@
+package com.google.code.genclipse.debug.core.launching;
+
+
+/**
+ * This interface is just a 'marker' interface, containing String-Constants that
+ * have to be shared throughout the whole GEclipse-application. 
+ * @author Christoph
+ *
+ */
+public interface EbuildDebuggingConstants {
+	
+	
+	public static final String EBUILD_NAME="EBUILD_NAME";
+	public static final String EBUILD_NAMES="EBUILD_NAMES";
+	public static final String EBUILD_CATEGORY="EBUILD_CATEGORY";
+	public static final String EBUILD_CATEGORIES="EBUILD_CATEGORIES";
+	public static final String EBUILD_CLEAN="EBUILD_CLEAN";
+	public static final String EBUILD_FETCH="EBUILD_FETCH";
+	public static final String EBUILD_DIGEST="EBUILD_DIGEST";
+	public static final String EBUILD_MANIFEST="EBUILD_MANIFEST";
+	public static final String EBUILD_UNPACK="EBUILD_UNPACK";
+	public static final String EBUILD_COMPILE="EBUILD_COMPILE";
+	public static final String EBUILD_TEST="EBUILD_TEST";
+	public static final String EBUILD_PREINST="EBUILD_PREINST";
+	public static final String EBUILD_INSTALL="EBUILD_INSTALL";
+	public static final String EBUILD_POSTINSTALL="EBUILD_POSTINSTALL";
+	public static final String EBUILD_COMMANDS="EBUILD_COMMANDS";
+	public static final String EBUILD_CONSOLENAME="GEclipse-Console";
+}
